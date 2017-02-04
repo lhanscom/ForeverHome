c8481ef96c6cc69b79d160f2f32c8fdf3b50b508
POWERCOREDOMAIN\lukeh <lukeh@mingleanalytics.com>
2017-02-04 02:14:34 -0500

    ugly cache bust

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace ForeverHomeNg
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site-{version}.css"));
         }
     }
 }
